c13041f6c70e4e52d805b64163b950a1cd421594
Joseph E. S. Abrams <joe@pop-os.localdomain>
2023-10-25 14:22:46 -0600

    much needed refactoring in progress

--- custom_modules/cryo_parameters.cpp
+++ custom_modules/cryo_parameters.cpp
@@ -1,1 +1,38 @@
 #include "./cryo_parameters.h"
+
+using namespace cryo_parameters;
+Cryo_Parameters::Cryo_Parameters(Cell* pCell)
+{
+  // if using XML value to specify a condition
+  simulation_selected=parameters.ints("selected_simulation");
+  // state parameters for Cryo, should be defined in custom_data for cell
+  surface_area=pCell->custom_data["surface_area"];
+  next_water_volume=0.0;//for ABM 2nd order
+  solid_volume=pCell->custom_data["initial_volume"]*pCell->custom_data["Vb_fraction"];
+  solute_volume=pCell->custom_data["initial_solute_volume"];//1449.82;
+  water_volume=pCell->custom_data["initial_volume"]-solid_volume-solute_volume;
+  use_virial=true;//ternary use, quatenary dont
+  dVw=0.0;//cell water flux
+  previous_dVw=0.0;
+  solute_moles.resize(microenvironment.number_of_densities(),0.0);
+  next_solute_moles.resize(microenvironment.number_of_densities(),0.0);
+  dN.resize(microenvironment.number_of_densities(),0.0); //cell mole flux of solutes
+  previous_dN.resize(microenvironment.number_of_densities(),0.0);//previous mole flux of solutes
+
+  exterior_osmolality=binary_virial(parameters.doubles("initial_EG_only_molarity"),"EG");//total exterior osmolality salt+CPA (mole/kg)
+  interior_osmolality=exterior_osmolality;// total internal osmolality salt+CPA
+  interior_molarity.resize(microenvironment.number_of_densities(),0.0);//1.68 is the kdiss for NaCl from virial
+  exterior_molarity=interior_molarity;
+  interior_component_molality=interior_molarity;
+  exterior_component_molality=exterior_molarity;
+  Ps.resize(microenvironment.number_of_densities(),0.0);
+  Lp=0.0;
+  solute_uptake.resize(microenvironment.number_of_densities(),0.0);
+  solute_volume=0.0;
+  water_uptake=0.0;
+  toxicity=0.0; 
+  previous_radius=0; 
+  //constants available to every spring cell, future version could have changing temp
+  uptake_voxels={};
+	return; 
+} 
--- custom_modules/cryo_parameters.h
+++ custom_modules/cryo_parameters.h
@@ -1,4 +1,6 @@
-
+/* Author: Joseph E.S. Abrams
+ * Please cite if you use this code
+ * This is a library of functions and paramaters for simualting cells and tissue undergoing cryopreservation*/
 
 #ifndef __CRYO_PARAMETERS_H__
 #define __CRYO_PARAMETERS_H__
@@ -10,9 +12,62 @@
 #include <fstream>
 #include <iostream>
 #include <omp.h>
+#include <vector>
 using namespace BioFVM;
 using namespace PhysiCell;
 
+namespace cryo_parameters{
+
+class Cryo_Parameters
+{
+ private:
+ public:
+  int simulation_selected;// if using XML value to specify a condition
+  //constants available to every spring cell, future version could have changing temp
+  double temperature;
+  const double gas_constant=0.08205;
+  /////paramaters for unit conversions using the virial equation
+    ///
+  const std::vector <double> solute_specific_volume={0.01661,0.0557414,0.0730903,0.01661};//{HM,EG,GLY,PBS} in um^3/femptomole (l/mole)
+  const std::vector <double> kdiss={1.678,1.0,1.0,1.678};//{HM,EG,GLY,PBS} virial ionic constant
+  const std::vector <double> virial_B={0.044,0.037,0.023,0.044};
+  const std::vector <double> virial_C={0.0,0.0,-0.001,0.0};
+  const std::vector <double> molar_mass={58.44,62.07,92.09,58.44};// note HM and PBS can have different molar masses depending on fomulation (especially with disodium phosphate) but we treat them as NaCl
+  /////////////////////////////////////////////////////////////  
+  double solute_volume; //total volume taken up by the free moles of solute in the cell
+  double surface_area; 
+  double next_water_volume;//for ABM 2nd order stores the next value of water volume
+  double water_volume;//current water volume 
+  double solid_volume;//non-osmotically active volume of cell
+  double dVw;//cell water flux
+  double previous_dVw;
+  double previous_radius;
+  std::vector<double> solute_moles;
+  std::vector<double> next_solute_moles;
+  std::vector<double> uptake;//molar uptake/secretion of solutes
+  std::vector<double> dN;//cell mole flux of solutes
+  std::vector<double> previous_dN;//previous mole flux of solutes
+  double exterior_osmolality;//total exterior osmolality salt+CPA (mole/kg)
+  double interior_osmolality;// total internal osmolality salt+CPA
+  std::vector<double> interior_molarity;
+  std::vector<double> exterior_molarity;
+  std::vector<double> interior_component_molality;
+  std::vector<double> exterior_component_molality;
+  std::vector<double> Ps;
+  double Lp;
+  double toxicity;
+  std::vector <double> solute_uptake;
+  double water_uptake;
+  std::vector <int> uptake_voxels;
+  bool use_virial;
+	Cryo_Parameters(Cell* pCell); 
+}; 
+
+
+
+
+
+};//namespace cryo_parameters
 
 
 
